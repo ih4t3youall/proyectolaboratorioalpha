e896873d076ec2ff88b84271fb0877a8ccd8ccd0
developproyectos@gmail.com <developproyectos@gmail.com@9cc55ba3-cab2-f354-bbb5-111988e1eb52>
2014-10-14 14:59:18 +0000

    se arreglo el eliminar producto

--- trunk/proyectLab/src/main/java/ar/com/lab/listeners/ListenerEliminarProducto.java
+++ trunk/proyectLab/src/main/java/ar/com/lab/listeners/ListenerEliminarProducto.java
@@ -4,10 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 
 import ar.com.lab.bo.ProductoBO;
 import ar.com.lab.objetos.Producto;
 import ar.com.lab.spring.SpringContext;
+import ar.com.lab.vistas.VistaEliminar;
 
 public class ListenerEliminarProducto implements ActionListener {
 
@@ -15,19 +17,22 @@ public class ListenerEliminarProducto implements ActionListener {
 	
 	private ProductoBO productoBO = (ProductoBO) SpringContext.getContext()
 			.getBean("productoBO");
+	private VistaEliminar vistaEliminar;
 	
-	
-	public ListenerEliminarProducto(JComboBox<Producto> productos){
+	public ListenerEliminarProducto(JComboBox<Producto> productos,VistaEliminar vistaEliminar){
 		
 		this.productos = productos;
-		
+		this.vistaEliminar = vistaEliminar;
 	}
 	
+
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		productoBO.eliminarProducto((Producto)productos.getSelectedItem());
-		
+		Producto producto =(Producto)productos.getSelectedItem();
+		productoBO.eliminarProducto(producto);
+		vistaEliminar.refresh();
+		JOptionPane.showMessageDialog(null, "El producto "+producto.getNombre()+" ah sido eliminado exitosamente.");
 		
 
 	}
